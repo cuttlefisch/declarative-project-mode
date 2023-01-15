2f9d0db84ecdd4ba894baad6b1a0251f056dc8ff
Hayden Stanko <33015950+cuttlefisch@users.noreply.github.com>
2023-01-14 17:38:20 -0800

    Support Yaml and JSON, Treemacs Workspace Assignment (#1)
    * support treemacs workspace. docstrings, cleanup
    
    * add missing readme contents
    
    * handle null lists hackily, update readme
    
    * correct keybinding
    
    * Create main.yml
    
    Trying out elisp linting.
    
    * populate workflow some more
    
    * populate job
    
    * switch to linting check
    
    * unsuccessful troubleshooting tries
    
    * remove dependencies & actions until worthwhile

--- declarative-project-mode.el
+++ declarative-project-mode.el
@@ -5,10 +5,10 @@
 ;; Author: Hayden Stanko <hayden@cuttle.codes>
 ;; Maintainer: Hayden Stanko <hayden@cuttle.codes>
 ;; Created: January 13, 2023
-;; Modified: January 13, 2023
-;; Version: 0.0.1
+;; Modified: January 14, 2023
+;; Version: 0.0.2
 ;; Keywords: project management, dependency management, declarative syntax, emacs minor-mode.
-;; ;; Homepage: https://github.com/cuttlefisch/declarative-project-mode
+;; Homepage: https://github.com/cuttlefisch/declarative-project-mode
 ;; Package-Requires: ((emacs "26.1"))
 ;;
 ;; This program is free software; you can redistribute it and/or modify
@@ -32,57 +32,87 @@
 ;; "required-resources", "project-deps", "project-local-files", "project-copy-files"
 ;;
 ;; Keybindings:
-;;   - `C-c i`: Run the install-project command
+;;   - `C-c C-c i`: Run the install-project command
 ;;
 ;;; Code:
 (require 'json)
+(require 'yaml-mode)
+(require 'treemacs)
 
-(defun declarative-project--check-required-resources ()
-  (let ((required-resources (gethash 'required-resources project-resources)))
+(defun declarative-project--check-required-resources (project-resources)
+  "Warn if any resources labeled required in PROJECT-RESOURCES are missing."
+  (when-let ((required-resources (gethash 'required-resources project-resources)))
     (seq-map (lambda (resource)
                (unless (file-exists-p resource)
-                 (message (concat "Missing required resource: " resource))))
+                 (warn (concat "Missing required resource: " resource))))
              required-resources)))
 
-(defun declarative-project--install-project-dependencies ()
-  (let ((project-deps (gethash 'project-deps project-resources)))
+(defun declarative-project--install-project-dependencies (project-resources)
+  "Clone any git dependencies locally in PROJECT-RESOURCES."
+  (when-let ((project-deps (gethash 'project-deps project-resources)))
     (seq-map (lambda (dep)
-               (unless (file-exists-p (file-name-nondirectory dep))
+               (unless (file-exists-p (file-name-base dep))
                  (shell-command (concat "git clone " dep))))
              project-deps)))
 
-(defun declarative-project--install-project-local-files ()
-  (let ((project-local-files (gethash 'project-local-files project-resources)))
-    (seq-map (lambda (file)
-               (copy-file file (concat default-directory file) t))
-             project-local-files)))
+(defun declarative-project--install-project-local-files (project-resources)
+  "Copy over any local files in PROJECT-RESOURCES."
+  (when-let ((project-local-files (gethash 'project-local-files project-resources)))
+    (when (car project-local-files)
+      (seq-map (lambda (file)
+                 (copy-file file (concat default-directory file) t))
+               project-local-files))))
 
-(defun declarative-project--install-project-copy-files ()
-  (let ((project-copy-files (gethash 'project-copy-files project-resources)))
-    (seq-map (lambda (file)
-               (let ((new-file-name (concat (file-name-sans-extension (file-name-nondirectory file)) "_modified" (file-name-extension (file-name-nondirectory file)))))
-                 (copy-file file (concat default-directory new-file-name) t)))
-             project-copy-files)))
+(defun declarative-project--install-project-copy-files (project-resources)
+  "Copy and mutate local files in PROJECT-RESOURCES."
+  (when-let ((project-copy-files (gethash 'project-copy-files project-resources)))
+    (when (car project-copy-files)
+      (seq-map (lambda (file)
+                 (let ((new-file-name (concat
+                                       (file-name-sans-extension (file-name-nondirectory file))
+                                       "_modified"
+                                       (file-name-extension (file-name-nondirectory file)))))
+                   (copy-file file (concat default-directory new-file-name) t)))
+               project-copy-files))))
+
+(defun declarative-project--apply-treemacs-workspaces (project-resources)
+  "Add project to any treemacs workspaces listed in PROJECT-RESOURCES."
+  (when-let ((project-workspaces (gethash 'treemacs-workspaces project-resources))
+             (project-file (gethash 'project-file project-resources)))
+    (seq-doseq (workspace project-workspaces)
+      (let ((project-name (or (gethash 'project-name project-resources) workspace)))
+        (treemacs-do-create-workspace workspace)
+        (treemacs-with-workspace (treemacs-find-workspace-by-name workspace)
+          (treemacs-do-add-project-to-workspace
+           (file-name-directory project-file)
+           project-name))))))
+
 
 (defun declarative-project--install-project ()
+  "Step step through project spec & apply any blocks found."
   (interactive)
   (let ((project-file (expand-file-name ".project" default-directory)))
     (when (file-exists-p project-file)
       (with-temp-buffer
         (insert-file-contents project-file)
-        (let ((json-object (json-read-from-string (buffer-string)))
-              (project-resources (make-hash-table :test 'equal)))
-          (mapc (lambda (pair) (puthash (car pair) (cdr pair) project-resources)) json-object)
-          (declarative-project--check-required-resources)
-          (declarative-project--install-project-dependencies)
-          (declarative-project--install-project-local-files)
-          (declarative-project--install-project-copy-files))))))
+        (let ((project-resources (or (yaml-parse-string (buffer-string)
+                                                        :null-object nil
+                                                        :sequence-type 'list)
+                                     (json-read-from-string (buffer-string)))))
+          (puthash 'project-file project-file project-resources)
+          (declarative-project--check-required-resources project-resources)
+          (declarative-project--install-project-dependencies project-resources)
+          (declarative-project--install-project-local-files project-resources)
+          (declarative-project--install-project-copy-files project-resources)
+          (declarative-project--apply-treemacs-workspaces project-resources)
+          (message "...Finished Installation!"))))))
 
 (define-minor-mode declarative-project-mode
   "Declarative Project mode."
-  :lighter " MyProj"
+  :lighter " DPM"
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-c i") 'install-project)
+            (define-key map (kbd "C-c C-c i")
+                        'declarative-project--install-project)
             map)
   (if declarative-project-mode
       (declarative-project--install-project)))
