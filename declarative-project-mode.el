383c4f0da8e7e6a3ebc4829719160bca8417db80
cuttlefisch <hayden@cuttle.codes>
2023-02-05 20:02:48 -0800

    fix dependencies

--- declarative-project-mode.el
+++ declarative-project-mode.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.4
 ;; Keywords: project management, dependency management, declarative syntax, emacs minor-mode.
 ;; Homepage: https://github.com/cuttlefisch/declarative-project-mode
-;; Package-Requires: ((emacs "25.1") (treemacs "2.10") (yaml-mode "0.0.15") (yaml "0.5.1") (magit "3.3.0") (ghub "3.5.1"))
+;; Package-Requires: ((emacs "25.1") (ghub "3.5.1") (magit "3.3.0") (treemacs "2.10") (yaml-mode "0.0.15") (yaml "0.5.1"))
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -42,9 +42,11 @@
 ;; PROJECT.yaml file
 ;;
 ;;; Code:
-(require 'yaml-mode)
-(require 'yaml)
+(require 'ghub)
+(require 'magit)
 (require 'treemacs)
+(require 'yaml)
+(require 'yaml-mode)
 
 (cl-defstruct declarative-project
   (name                 ""      :type string)
